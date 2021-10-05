a7713f899ffbeecf15db03dcc5ff344b87e1c08f
Kaito Hattori <kaichan394@gmail.com>
2021-10-05 22:29:37 +0900

    rollback media

--- app/model/media.go
+++ app/model/media.go
@@ -19,28 +19,45 @@ type Media struct {
 }
 
 func (m *Media) Upload(ctx *gin.Context) (*Video, error) {
+	ctxDB := DbConnection.WithContext(ctx.Request.Context())
+	tx := ctxDB.Begin()
+
 	video := Video{
 		Title:     m.Title,
 		UserId:    m.UserId,
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
-	if err := video.Insert(ctx); err != nil {
+	if err := tx.Create(&video).Error; err != nil {
+		// Rollback
+		tx.Rollback()
 		return nil, err
 	}
 
 	// Make working directory
 	dirPath, err := util.MediaUtil.MakeWorkingDirectory(video.Id)
 	if err != nil {
+		// Rollback
+		tx.Rollback()
+		// Delete working directory
+		util.MediaUtil.DeleteWorkingDirectory(*dirPath)
 		return nil, err
 	}
 	// Save video
 	videoFilePath, err := util.MediaUtil.SaveVideo(m.File, m.FileHeader, *dirPath)
 	if err != nil {
+		// Rollback
+		tx.Rollback()
+		// Delete working directory
+		util.MediaUtil.DeleteWorkingDirectory(*dirPath)
 		return nil, err
 	}
 	// Process video
 	if err := util.MediaUtil.ProcessVideo(ctx, video.Id, *videoFilePath); err != nil {
+		// Rollback
+		tx.Rollback()
+		// Delete working directory
+		util.MediaUtil.DeleteWorkingDirectory(*dirPath)
 		return nil, err
 	}
 	// Delete working directory
@@ -48,8 +65,10 @@ func (m *Media) Upload(ctx *gin.Context) (*Video, error) {
 
 	select {
 	case <-ctx.Done():
+		tx.Rollback()
 		return nil, ctx.Err()
 	default:
+		tx.Commit()
 		return &video, nil
 	}
 }
