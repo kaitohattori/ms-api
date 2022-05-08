2561af7fc9e5bff50612e64c35cc519d44551cab
Kaito Hattori <kaichan394@gmail.com>
2022-05-08 11:31:29 +0900

    不要なcors設定を削除

--- main.go
+++ main.go
@@ -39,22 +39,6 @@ func main() {
 	StartServer()
 }
 
-func CorsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(200)
-		} else {
-			c.Next()
-		}
-	}
-}
-
 func StartServer() {
 	engine := gin.Default()
 
@@ -63,7 +47,6 @@ func StartServer() {
 		config.Config.Auth0Identifier,
 		config.Config.Auth0Domain,
 	)
-	engine.Use(CorsMiddleware())
 
 	// Timeout
 	engine.Use(
